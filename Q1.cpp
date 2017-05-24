ad2edbc623b1e0067ad28906adb185302453ae80
Matineh Akhlaghinia <matineh.akhlaghinia.15@ucl.ac.uk>
2017-05-24 23:37:49 +0430

    update#1
    changes made according to the comments, passing arguments as const refs
    and making the name of the method more appropriate

--- Q1.cpp
+++ Q1.cpp
@@ -12,41 +12,41 @@ Apple, Pabble ⇒ False */
 using namespace std;
 
 
-bool mapIsEmpty(unordered_map<char, int> freq_map) {
-  unordered_map<char, int>::iterator it;
+bool mapHasOnlyZeroes(const unordered_map<char, int>& freq_map) {
+  unordered_map<char, int>::const_iterator it;
   for(it = freq_map.begin(); it != freq_map.end(); it++) {
-  	if(it->second != 0)
+    if(it->second != 0)
       return false;
   }
   return true;
 }
 
-bool isPermutation(string s1, string s2) {
+bool isPermutation(const string& s1, const string& s2) {
   unordered_map<char, int> frequency_map;
-  transform(s1.begin(), s1.end(), s1.begin(), ::tolower); 
-  transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
   if(s1.length() != s2.length())
-  	return false;
+    return false;
   for(int i = 0; i < s1.length(); i++) {
     frequency_map[s1[i]]++;
   }
   for(int j = 0; j < s2.length(); j++) {
-  	if(frequency_map.find(s2[j]) != frequency_map.end())
-  	  frequency_map[s2[j]]--;
-  	else
-  	  return false;
+    if(frequency_map.find(s2[j]) != frequency_map.end())
+      frequency_map[s2[j]]--;
+    else
+      return false;
   }
   return mapIsEmpty(frequency_map);
 }
 
 int main() {
-	string s1, s2;
-	getline (cin,s1);
-	getline (cin,s2);
-	if(isPermutation(s1, s2))
-		cout << "YES" << endl;
-	else
-		cout << "NO" << endl;
+  string s1, s2;
+  getline (cin,s1);
+  getline (cin,s2);
+  transform(s1.begin(), s1.end(), s1.begin(), ::tolower); 
+  transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+  if(isPermutation(s1, s2))
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
 
-	return 0;
+  return 0;
 }
